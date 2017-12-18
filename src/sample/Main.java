d080033810fd0e5ba7a057bb9c6a59b060951a0e
Karishnu Poddar <karishnu@gmail.com>
2017-12-18 06:59:33 +0530

    libs and maps

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Flight Software");
+        primaryStage.setScene(new Scene(root, 1500, 700));
         primaryStage.show();
     }
 
